0cd24d737b28a355eb0d57921c1eca16c07edfa0
Medialoha <support@medialoha.net>
2014-02-27 08:38:48 +0100

    Fix install script problem

--- install/install.php
+++ install/install.php
@@ -9,11 +9,15 @@ $mUpdateHelper = new UpdateHelper();
 
 $mUpdateHelper->begin();
 
+$mUpdateHelper->applySQLUpdateFile('db-install.sql');
+
+$mUpdateHelper->exitOnError();
+
 $mUpdateHelper->printStartNextStepMsg('Populate increments table');
 
 // populate table increments
 for ($i=0; $i<=180; ++$i) {
-	$mUpdateHelper->execSQL('INSERT INTO '.DBHelper::getTblName(TBL_INCREMENTS).'('.INC_VALUE.') VALUES ('.$i.');');
+	$mUpdateHelper->execSQL('INSERT INTO '.TBL_INCREMENTS.'('.INC_VALUE.') VALUES ('.$i.');');
 }
 
 $mUpdateHelper->printEndStepMsg(true, null, true);
